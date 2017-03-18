9c7a4ce6e55b3f37ad4ba76d8e05a1533025646a
Rahilka Simonova <rahilka.simonova@mi-c3.com>
2017-03-18 20:43:55 +0100

    Call Apply Bind

--- app.js
+++ app.js
@@ -1,41 +1,55 @@
-function sayHiLater() {
-
-  var greeting = 'Hi!';
-
-  setTimeout(function() {
-
-    console.log(greeting);
-
-  }, 3000);
-
+var person = {
+  firstname: 'Rahilka',
+  lastname: 'Simonova',
+  getFullName: function() {
+    var fullname = this.firstname + ' ' + this.lastname;
+    return fullname;
+  }
 }
 
-sayHiLater();
-
-//jQuery uses function expressions and first class functions!
-// $("button").click(function() {
-//
-// });
-
-
-function tellMeWhenDone(callback) {
-
-  var a = 1000;
-  var b = 2000;
-
-  callback();
-
+var logName = function(lang1, lang2) {
+  console.log('Logged: ' + this.getFullName());
+  console.log('Arguments: ', arguments);
+  console.log('-----------');
 }
 
-tellMeWhenDone(function() {
+var logPersonName = logName.bind(person);
+//bind creates copy of the function, without executing
 
-  console.log('Im done');
+logPersonName('en');
 
-});
+//call executes the function
+logName.call(person, 'en', 'es');
+//apply executes the function too
+logName.apply(person, ['en', 'es']);
+
+//THE ONLY DIFFERENCE between CALL and APPLY is
+//that apply wants the argument in an array
+
+(function(lang1, lang2) {
+  console.log('Logged: ' + this.getFullName());
+  console.log('Arguments: ', arguments);
+  console.log('-----------');
+}).apply(person, ['en', 'mk']);
 
 
-tellMeWhenDone(function() {
+//function borrowing
 
-  console.log('All done..');
+var person2 = {
+  firstname: 'Ljubica',
+  lastname: 'Simonova'
+}
 
-});
+console.log(person.getFullName.apply(person2));
+
+
+// functon currying
+
+function multiply(a, b) {
+  return a * b;
+}
+
+var multipleByTwo = multiply.bind(this, 2);
+//THE VARIABLE a WILL ALWAYS BE 2
+
+console.log(multipleByTwo(5));
