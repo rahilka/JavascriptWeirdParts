84863ef143bde1a536076db674d9dfbdac359f08
Rahilka Simonova <rahilka.simonova@mi-c3.com>
2017-03-18 18:35:03 +0100

    Closures part1

--- app.js
+++ app.js
@@ -1,35 +1,11 @@
-//function statement
-function greet(name) {
-  console.log('Hello ' + name);
+function greet(whatToSay) {
+
+  return function(name) {
+    console.log(whatToSay + ' ' + name);
+  }
+
+
 }
 
-greet('Rahilka');
-
-//using a function expression
-var greetFunc = function(name) {
-  console.log('Hello ' + name);
-};
-
-greetFunc('Rahilka');
-
-//using and Immediately Invoked FUnction expression// IIFE
-var greeting = function(name) {
-
-  return ('Hello ' + name);
-
-}('Rahilka');
-
-console.log(greeting);
-
-
-
-
-(function(global, name) {
-
-  var greetings = 'Hello ';
-  global.greetings = 'Heloooo';
-  console.log(greetings + name);
-
-}(window, 'Rahilka'));  //IIFE
-
-console.log(greetings);
+var sayHi = greet('HI');
+sayHi('Rahilka');
