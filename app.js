8179c100a1bde8fe1a313bff506ab202b0d6797b
Rahilka Simonova <rahilka.simonova@mi-c3.com>
2017-03-19 12:23:39 +0100

    Reflection and extend

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 var person = {
-
   firstname: 'Default',
   lastname: 'Default',
   getFullName: function() {
     return this.firstname + ' ' + this.lastname;
   }
-
 }
 
 var john = {
@@ -13,17 +11,27 @@ var john = {
   lastname: 'Doe'
 }
 
-//don't do this EVER!!!
-//for demo purposes only!!!
-
 john.__proto__ = person;
-console.log(john.getFullName());
-console.log(john.firstname);
 
-var jane = {
-  firstname: 'Jane'
+for (var prop in john) {
+  if(john.hasOwnProperty(prop)) {
+    console.log(prop + ': ' + john[prop]);
+  }
 }
 
-jane.__proto__ = person;
-console.log(jane.getFullName());
-//expected: Jane Default
+var jane = {
+  address: '111 Main St.',
+  getFormalFullName: function() {
+    return this.lastname + ', ' + this.firstname;
+  }
+}
+
+var jim = {
+  getFirstName: function () {
+    return firstname;
+  }
+}
+
+_.extend(john, jane, jim);
+
+console.log(john);
