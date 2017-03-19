8bc684fca909c842cb7319655bef19ca01bc8379
Rahilka Simonova <rahilka.simonova@mi-c3.com>
2017-03-19 14:51:21 +0100

    Built in function constructors

--- app.js
+++ app.js
@@ -1,25 +1,10 @@
-function Person(firstname, lastname) {
-
-  console.log(this);
-  this.firstname = firstname;
-  this.lastname = lastname;
-  console.log('this function is invoked');
-
+String.prototype.isLengthGreaterThan = function(limit) {
+  return this.length > limit;
 }
 
-Person.prototype.getFullName = function() {
-  return this.firstname + ' ' + this.lastname;
+console.log("Jogn".isLengthGreaterThan(3));
+
+
+Number.prototype.isPositive = function() {
+  return this>0;
 }
-
-var john = new Person('John', 'Doe');
-console.log(john);
-
-var jane = new Person('Jane', 'Doe');
-console.log(jane);
-console.log(jane.getFullName());
-
-Person.prototype.getFormalFullName = function() {
-  return this.lastname + ', ' + this.firstname;
-}
-
-console.log(john.getFormalFullName());
